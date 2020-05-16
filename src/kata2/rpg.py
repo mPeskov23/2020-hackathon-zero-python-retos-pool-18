555e3f65f2f74d1e4c6fe1d1baecee11cc26deff
Mikhail Peskov <50680267+mPeskov23@users.noreply.github.com>
2020-05-16 21:06:46 +0200

    KATA2

--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -3,11 +3,22 @@
 import random
 import string
 
-def RandomPasswordGenerator(passLen=10):
-    #
-    #
-    
-    #
-    #
+def RandomPasswordGenerator(passLen):
+    generatedPassword = ""
+    for i in range(passLen+1):
+        chartype = random.randint(1,4)
+        if chartype == 1:
+            new_char = random.choice(string.ascii_lowercase)
+            generatedPassword = generatedPassword + new_char
+        elif chartype == 2:
+            new_char = random.choice(string.ascii_uppercase)
+            generatedPassword = generatedPassword + new_char
+        elif chartype == 3:
+            new_char = random.choice(string.digits)
+            generatedPassword = generatedPassword + new_char
+        else:
+            new_char = random.choice(string.punctuation)
+            generatedPassword = generatedPassword + new_char
 
-    return ""
+
+    return generatedPassword
